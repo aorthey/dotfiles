5c4c5ba84abc832f11a74535240e7205d2ef4338
Andreas Orthey <andreas.orthey@gmx.de>
2014-07-14 11:47:40 +0200

    update on install script

--- install.sh
+++ install.sh
@@ -8,9 +8,14 @@ echo "Installing BASHRC script in root directory..."
 sudo cp ~/.bashrc /root
 sudo mkdir /root/.bash
 sudo cp ~/.bash/apt_tab_completion /root/.bash
+mkdir -p .vim/bundle/
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Installing VLC|DROPBOX..."
-sudo apt-get install nautilus-dropbox vlc 
+sudo apt-get install vlc 
 echo "Installing MISC programs..."
-sudo apt-get install htop nautilus-open-terminal texlive-full curl libmp3lame0 cmake-curses-gui silversearcher-ag youtube-dl 
+sudo apt-get install htop nautilus-open-terminal curl libmp3lame0 cmake-curses-gui silversearcher-ag youtube-dl 
 echo "disabling keypad"
 echo "run xinput list; xinput set-prop 12 "Device Enabled" 0"
+
+echo "Installing LATEX essentials"
+sudo apt-get install texlive-full
