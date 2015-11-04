51ea0d41461d0c1178024863140eb77242d39ebd
Andreas Orthey <andreas.orthey@gmx.de>
2015-11-04 16:02:07 -0500

    volume

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -34,76 +34,30 @@ myBorderWidth   = 6
 myModMask       = mod1Mask
 strgMask        = xK_Control_L
  
--- NOTE: from 0.9.1 on numlock mask is set automatically. The numlockMask
--- setting should be removed from configs.
---
--- You can safely remove this even on earlier xmonad versions unless you
--- need to set it to something other than the default mod2Mask, (e.g. OSX).
---
--- The mask for the numlock key. Numlock status is "masked" from the
--- current modifier status, so the keybindings will work with numlock on or
--- off. You may need to change this on some systems.
---
--- You can find the numlock modifier by running "xmodmap" and looking for a
--- modifier with Num_Lock bound to it:
---
--- > $ xmodmap | grep Num
--- > mod2        Num_Lock (0x4d)
---
--- Set numlockMask = 0 if you don't have a numlock key, or want to treat
--- numlock status separately.
---
--- myNumlockMask   = mod2Mask -- deprecated in xmonad-0.9.1
-------------------------------------------------------------
- 
- 
--- The default number of workspaces (virtual screens) and their names.
--- By default we use numeric strings, but any string may be used as a
--- workspace name. The number of workspaces is determined by the length
--- of this list.
---
--- A tagging example:
---
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
---
 myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
- 
--- Border colors for unfocused and focused windows, respectively.
---
 myNormalBorderColor  = "#000000"
 myFocusedBorderColor = "#ff0000"
  
 ------------------------------------------------------------------------
--- Key bindings. Add, modify or remove key bindings here.
---
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
  
-
-    --
-    -- mod-[1..9], Switch to workspace N
-    --
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
-    --
+    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+
     [((m .|. modm, k), windows $ f i)
          | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
      ++
  
-    --
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
          | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
     ++
-    -- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
- 
     , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
- 
     ,  ((modm .|. shiftMask, xK_f), spawn "firefox")
     ,  ((modm .|. shiftMask, xK_v), spawn "vlc")
     ,  ((modm .|. shiftMask, xK_n), spawn "nautilus")
@@ -130,8 +84,10 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm              , xK_comma ), sendMessage Expand)
     , ((modm              , xK_period), sendMessage Shrink)
     , ((modm .|. shiftMask, xK_comma ), sendMessage MirrorExpand)
-   , ((modm .|. shiftMask, xK_period), sendMessage MirrorShrink)
-   , ((modm .|. shiftMask, xK_b), spawn "gnome-session-quit --power-off --no-prompt --force")
+    , ((modm .|. shiftMask, xK_period), sendMessage MirrorShrink)
+    , ((modm .|. shiftMask, xK_b     ), spawn "gnome-session-quit --power-off --no-prompt --force")
+    , ((modm              , xK_Up    ), spawn "amixer -D pulse sset Master 5%+")
+    , ((modm              , xK_Down  ), spawn "amixer -D pulse sset Master 5%-")
 
     -- Push window back into tiling
     --, ((modm, xK_f), withFocused $ windows . W.shiftMaster)
@@ -145,10 +101,6 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_l     ), lShiftKeyWindowBehaviour)
     , ((modm .|. shiftMask, xK_j     ), jShiftKeyWindowBehaviour)
     , ((modm .|. shiftMask, xK_k     ), kShiftKeyWindowBehaviour)
-
-    -- Quit xmonad
-    --, ((modm .|. shiftMask, xK_w     ), io (exitWith ExitSuccess))
-    -- Restart xmonad
     , ((modm .|. shiftMask              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
     ]where
 	hKeyWindowBehaviour = withFocused (\w -> do
@@ -221,23 +173,6 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
  
-------------------------------------------------------------------------
--- Layouts:
- 
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- * NOTE: XMonad.Hooks.EwmhDesktops users must remove the obsolete
--- ewmhDesktopsLayout modifier from layoutHook. It no longer exists.
--- Instead use the 'ewmh' function from that module to modify your
--- defaultConfig as a whole. (See also logHook, handleEventHook, and -- startupHook ewmh notes.)
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
---
-
 
 --myLayout = windowNavigation (Tall 1 (3/100) (1/2)) ||| Full ||| desktopLayoutModifiers (Tall 1 0.03 0.5) ||| tiled ||| Full
 myLayout = windowNavigation (desktopLayoutModifiers (tiled) ) ||| Full
@@ -255,51 +190,6 @@ myLayout = windowNavigation (desktopLayoutModifiers (tiled) ) ||| Full
     delta   = 3/100
 
  
-------------------------------------------------------------------------
--- Window rules:
- 
--- Execute arbitrary actions and WindowSet manipulations when managing
--- a new window. You can use this to, for example, always float a
--- particular program, or have a client always appear on a particular
--- workspace.
---
--- To find the property name associated with a program, use
--- > xprop | grep WM_CLASS
--- and click on the client you're interested in.
---
--- To match on the WM_NAME, you can use 'title' in the same way that
--- 'className' and 'resource' are used below.
---
---myManageHook = composeAll . concat $
---	[ [ resource  =? r --> doIgnore                    | r <- myIgnores ]
---	, [ className =? c --> doShift (myWorkspaces !! 1) | c <- myWebS    ]
---	, [ className =? c --> doShift (myWorkspaces !! 2) | c <- myCodeS   ]
---	, [ className =? c --> doShift (myWorkspaces !! 3) | c <- myGfxS    ]
---	, [ className =? c --> doShift (myWorkspaces !! 4) | c <- myChatS   ]
---	, [ className =? c --> doShift (myWorkspaces !! 9) | c <- myAlt3S   ]
---	, [ className =? c --> doCenterFloat               | c <- myFloatCC ]
---	, [ name      =? n --> doCenterFloat               | n <- myFloatCN ]
---	] where
---		name      = stringProperty "WM_NAME"
---		myIgnores = []
---		myWebS    = ["Chromium", "Firefox", "Opera"]
---		myCodeS   = ["NetBeans IDE 7.3"]
---		myChatS   = ["Pidgin", "Skype"]
---		myGfxS    = ["Gimp", "gimp", "GIMP"]
---		myAlt3S   = ["Amule", "Transmission-gtk"]
---		myFloatCC = ["MPlayer", "mplayer2", "File-roller", "zsnes", "Gcalctool", "Exo-helper-1"
---			    , "Gksu", "Galculator", "Nvidia-settings", "XFontSel", "XCalc", "XClock"
---			    , "Ossxmix", "Xvidcap", "Wicd-client.py"]
---		myFloatCN = ["Choose a file", "Open Image", "File Operation Progress", "Firefox Preferences"
---			    , "Preferences", "Search Engines", "Set up sync", "Passwords and Exceptions"
---			    , "Autofill Options", "Rename File", "Copying files", "Moving files"
---			    , "File Properties", "Replace", "Quit GIMP", "Change Foreground Color"
---			    , "Change Background Color", ""]
---		myFloatSN = ["Event Tester"]
---		myFocusDC = ["Event Tester", "Notify-osd"]
---		keepMaster c = assertSlave <+> assertMaster where
---			assertSlave = fmap (/= c) className --> doF W.swapDown
---			assertMaster = className =? c --> doF W.swapMaster
 myManageHook = composeAll
     [ manageHook gnomeConfig
     --, className =? "tilda"           --> doFloat
@@ -315,51 +205,6 @@ myManageHook = composeAll
     --, className =? "emulator-arm" --> doFloat
     ]
       where role = stringProperty "WM_WINDOW_ROLE"
---
-------------------------------------------------------------------------
--- Event handling
- 
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
---
--- * NOTE: EwmhDesktops users should use the 'ewmh' function from
--- XMonad.Hooks.EwmhDesktops to modify their defaultConfig as a whole.
--- It will add EWMH event handling to your custom event hooks by
--- combining them with ewmhDesktopsEventHook.
---
--- myEventHook = mempty
- 
-------------------------------------------------------------------------
--- Status bars and logging
- 
--- Perform an arbitrary action on each internal state change or X event.
--- See the 'XMonad.Hooks.DynamicLog' extension for examples.
---
---
--- * NOTE: EwmhDesktops users should use the 'ewmh' function from
--- XMonad.Hooks.EwmhDesktops to modify their defaultConfig as a whole.
--- It will add EWMH logHook actions to your custom log hook by
--- combining it with ewmhDesktopsLogHook.
---
--- myLogHook = return ()
- 
-------------------------------------------------------------------------
--- Startup hook
- 
--- Perform an arbitrary action each time xmonad starts or is restarted
--- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
--- per-workspace layout choices.
---
--- By default, do nothing.
---
--- * NOTE: EwmhDesktops users should use the 'ewmh' function from
--- XMonad.Hooks.EwmhDesktops to modify their defaultConfig as a whole.
--- It will add initialization of EWMH support to your custom startup
--- hook by combining it with ewmhDesktopsStartup.
---
---myStartupHook = return ()
- 
 
 ------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
