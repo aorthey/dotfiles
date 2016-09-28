6997e55661ad5f800d8402d0e279b35cdcfef16b
Andreas Orthey <andreas.orthey@gmx.de>
2016-09-28 11:35:52 -0400

    minor

--- install.sh
+++ install.sh
@@ -9,11 +9,12 @@ sudo cp ~/.bashrc /root
 sudo mkdir /root/.bash
 sudo cp ~/.bash/apt_tab_completion /root/.bash
 mkdir -p .vim/bundle/
+sudo apt-get install vlc vim htop nautilus-open-terminal curl libmp3lame0 \
+cmake-curses-gui silversearcher-ag youtube-dl xmonad libghc-xmonad-dev \
+libghc-xmonad-contrib-dev xmobar xcompmgr nitrogen stalonetray moreutils \
+consolekit ssh-askpass-gnome thunar terminator remmina gnome-panel 
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "Installing VLC|DROPBOX..."
-sudo apt-get install vlc 
-echo "Installing MISC programs..."
-sudo apt-get install htop nautilus-open-terminal curl libmp3lame0 cmake-curses-gui silversearcher-ag youtube-dl 
 echo "disabling keypad"
 echo "run xinput list; xinput set-prop 12 "Device Enabled" 0"
 
@@ -23,4 +24,4 @@ echo "Displaying date in time indicator"
 gsettings set com.canonical.indicator.datetime show-clock true
 gsettings set com.canonical.indicator.datetime show-day true
 gsettings set com.canonical.indicator.datetime show-date true
-
+echo "FIREFOX: install vimperator | noscript | adblockplus"
